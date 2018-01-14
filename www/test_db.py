e24790fcd2fe914c8cb86c1511bb29ad55fc19df
maxlv7 <admin@maxlv.org>
2018-01-14 22:47:09 +0800

    Update some files

--- www/test_db.py
+++ www/test_db.py
@@ -1,16 +1,12 @@
-import orm
-import asyncio
+import orm, asyncio
 from models import User, Blog, Comment
 
 loop = asyncio.get_event_loop()
-
 async def test():
-    await orm.create_pool(loop=loop,user="root",password="root",db="my_blog")
+	await orm.create_pool(loop, user='root', password='root', db='my_blog')
+	u = User(name='Test', email='121234', passwd='123456', image='about:blank')
+	await u.save()
 
-    u = User(name="li",passwd="123123",image="123")
-
-    await u.save()
 
 loop.run_until_complete(test())
-
-loop.close()
+# loop.close()
