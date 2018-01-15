de9d53c0d31fb6c9e4fa01a6e2f31d1072543b05
maxlv7 <admin@maxlv.org>
2018-01-15 10:22:09 +0800

    update date

--- www/test_db.py
+++ www/test_db.py
@@ -4,7 +4,7 @@ from models import User, Blog, Comment
 loop = asyncio.get_event_loop()
 async def test():
 	await orm.create_pool(loop, user='root', password='root', db='my_blog')
-	u = User(name='Test', email='121234', passwd='123456', image='about:blank')
+	u = User(id="1",name='Li', email='admin@maxlv.org', passwd='123456', image='about:blank')
 	await u.save()
 
 
